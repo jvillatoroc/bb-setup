6fe0ed0ec7f96faf9829fb3b0b55b4ef9c7cd1a4
Jonathan Villatoro <lacho8713@gmail.com>
2022-10-12 12:32:33 -0600

    modified recon directory, modified gospider and feroxbuster scanning options

--- recon.sh
+++ recon.sh
@@ -4,7 +4,8 @@
 
 PROJECT=$1
 
-cd ~/notas/02\ Proyectos/$PROJECT/
+mkdir ~/recon/
+cd ~/recon/$PROJECT/
 
 echo "running amass on $PROJECT\'s seed domains"
 for domain in `cat $PROJECT-seeds.md`; do
@@ -12,7 +13,7 @@ for domain in `cat $PROJECT-seeds.md`; do
 	# amass. If it does, skip and move on to the next seed
 	# domain
 	[[ ! -a $domain/$domain.txt ]] && \
-		mkdir $domain && \
+		mkdir -p $domain && \
 		amass enum -brute -o $domain/$domain.txt -d $domain -v
 
 	echo "running hakrawler on $domain"
@@ -22,8 +23,11 @@ done
 echo "running gospider and feroxbuster on subdomains"
 for domain in `cat $PROJECT-seeds.md`; do
 	for subdomain in `cat $domain/$domain.txt`; do
-		gospider -s "https://$subdomain" -o $subdomain/gospider --threads 20 --concurrent 10 --depth 5 --other-source --include-subs
-		feroxbuster -u $subdomain -k --redirects --depth 5 --extract-links --force-recursion -w ~/tools/SecLists/Discovery/Web-Content/raft-medium-words.txt -o $domain/$subdomain-feroxbuster.txt -vv
+		gospider -s "https://$subdomain" -o $subdomain/gospider --threads 20 --concurrent 10 --depth 5 --other-source --include-subs --robots
+		feroxbuster -u $subdomain -k --redirects --depth 5 --extract-links --force-recursion -w ~/tools/commonspeak2-wordlists/wordswithext/aspx.txt -o $domain/$subdomain-feroxbuster-aspx.txt -vv
+		feroxbuster -u $subdomain -k --redirects --depth 5 --extract-links --force-recursion -w ~/tools/commonspeak2-wordlists/wordswithext/jspx.txt -o $domain/$subdomain-feroxbuster-jsp.txt -vv
+		feroxbuster -u $subdomain -k --redirects --depth 5 --extract-links --force-recursion -w ~/tools/commonspeak2-wordlists/wordswithext/php.txt -o $domain/$subdomain-feroxbuster-php.txt.txt -vv
+		feroxbuster -u $subdomain -k --redirects --depth 5 --extract-links --force-recursion -w ~/tools/commonspeak2-wordlists/wordswithext/words.txt -o $domain/$subdomain-feroxbuster-words.txt -vv
 	done
 done
 
