ee4c86d7a8f1c70c083815d5e9d7cdbb7ad92401
Jonathan Villatoro <lacho8713@gmail.com>
2022-10-23 01:49:51 -0600

    added separate asn_domains, asn_ips, enum_subdomains recon scripts

---
+++ asn_ips.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+[[ -z $@ ]] && echo "I need a project name and to have a PROJECT-ASNs.md file!" && exit 1
+
+PROJECT=$1
+
+cd ~/recon/$PROJECT/
+
+echo "getting IPs from $PROJECT's ASN numbers"
+for ASN in `cat $PROJECT-ASNs.md`; do
+	mkdir $ASN
+	amass intel -asn $ASN -ip -o $ASN/$ASN-ips
+done
---
+++ enum_subdomains.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+[[ -z $@ ]] && echo "give me a project name, and make sure to create a PROJECT-seeds.md file inside there!" && exit 1
+
+PROJECT=$1
+
+cd ~/recon/$PROJECT/
+
+echo "running amass on $PROJECT's seed domains"
+for domain in `cat $PROJECT-seeds.md`; do
+	# if the file doesn't exist, create the directory and run
+	# amass. If it does, skip and move on to the next seed
+	# domain
+	[[ ! -a $domain/$domain.txt ]] && \
+		mkdir -p $domain && \
+		amass enum -brute -o $domain/$domain-subs -d $domain -v
+done
