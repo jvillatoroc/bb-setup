1088120834e161fe5d6e05c0ea4b7f9e8110c654
Jonathan Villatoro <lacho8713@gmail.com>
2022-11-23 23:57:45 -0600

    set up brute_subdomains to output traight to file, without directory

--- brute_subdomains.sh
+++ brute_subdomains.sh
@@ -8,10 +8,5 @@ cd ~/recon/$PROJECT/
 
 echo "running amass on $PROJECT's seed domains"
 for domain in `cat $PROJECT-seeds.md`; do
-	# if the file doesn't exist, create the directory and run
-	# amass. If it does, skip and move on to the next seed
-	# domain
-	[[ ! -a $domain/$domain.txt ]] && \
-		mkdir -p $domain && \
-		amass enum -brute -o $domain/$domain.txt -d $domain -w ~/tools/jhaddix/all.txt -v
+	amass enum -brute -o $domain.txt -d $domain -w ~/tools/jhaddix/all.txt -v
 done
